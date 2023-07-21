97261aef446c94330721aa588b79e99c2c01087b
ddowy <92402470+ddowy@users.noreply.github.com>
2023-07-21 06:40:32 -0500

    Added images folder in public and put logo.svg in it. Added logo to header along with a little styling to make it fit in nice

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import styles from './header.module.css'
 export default function Header() {
     return (
       <header className={styles.header}>
-        <div className={styles.logoContainer}>LOGO</div>
+        <div className={styles.logoContainer}>
+          <img className={styles.logo} src="/images/logo.svg" alt="Logo for St. Pauls homeless Youth Advisory Board" />
+        </div>
         <nav className={styles.nav}>
           <ul className={styles.navul}>
             <li className={styles.navli}>
