b2864cf08de1fb3280202a8f5c7a0f83b4c47fcd
ddowy <92402470+ddowy@users.noreply.github.com>
2023-07-25 21:34:55 -0500

    Cube is positioned correctly on canvas and added in css shadow to make blur affect on navbar

--- src/components/CubeImageDisplay/components/Canvas/Canvas.js
+++ src/components/CubeImageDisplay/components/Canvas/Canvas.js
@@ -5,6 +5,8 @@ import styles from './canvas.module.css'
 import * as THREE from 'three'
 import { useEffect, useRef } from 'react'
 import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitControls.js'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+
 
 export default function Canvas() {
 
@@ -22,15 +24,12 @@ export default function Canvas() {
                 this._scene = new THREE.Scene()
                 this._threejs = new THREE.WebGLRenderer()
                 this._camera = new THREE.PerspectiveCamera(75, canvasParentRef.current.clientWidth / canvasParentRef.current.clientHeight, 0.1, 1000)
-                this._camera.position.set(0, 1, 20);
-
+                this._camera.position.set(0, 0, 1.35);
                 this._threejs.setSize(canvasParentRef.current.clientWidth, canvasParentRef.current.clientHeight)
                 canvasParentRef.current.appendChild( this._threejs.domElement );
 
-                // this._orbitControls = new OrbitControls(this._camera, canvasParentRef.current)
-                // this._orbitControls.update()
-
-                this._scene.background = new THREE.Color(0xffffff)
+                this._orbitControls = new OrbitControls(this._camera, canvasParentRef.current)
+                this._orbitControls.update()
 
                 this._AddBoxGeometry()
 
@@ -46,9 +45,10 @@ export default function Canvas() {
             }
 
             _AddBoxGeometry() {
-                const geometry = new THREE.BoxGeometry(10, 10, 10)
+                const geometry = new THREE.BoxGeometry(1, 1, 1)
                 const material = new THREE.MeshBasicMaterial({ color: 0x8a0c1b })
                 this._box = new THREE.Mesh(geometry, material)
+                this._box.position.setX(-0.5)
                 this._scene.add(this._box)
             }
 
@@ -59,12 +59,13 @@ export default function Canvas() {
                 this._threejs.setSize(canvasParentRef.current.clientWidth, canvasParentRef.current.clientHeight);   
             }
 
+
             _RAF() {
                 requestAnimationFrame(() => {
                     this._threejs.render(this._scene, this._camera)
-                    this._box.rotateX(0.01)
+                    this._box.rotateX(0.02)
                     this._box.rotateY(0.01)
-                    // this._orbitControls.update()
+                    this._orbitControls.update()
                     this._RAF()
                 })
             }
