3b5a99be111a3f86b959ad26360d9587964a4b30
ddowy <92402470+ddowy@users.noreply.github.com>
2023-07-29 23:44:17 -0500

    Added in cube model from blender with images already on faces. The model cube already has 3 animations as well which are float, turnright and turnleft. Cube is in a good position, slightly to the left of the middle of the screen with float animation replaying.

--- src/components/CubeImageDisplay/components/Canvas/Canvas.js
+++ src/components/CubeImageDisplay/components/Canvas/Canvas.js
@@ -1,7 +1,6 @@
 'use client'
 
 import styles from './canvas.module.css'
-
 import * as THREE from 'three'
 import { useEffect, useRef } from 'react'
 import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitControls.js'
@@ -14,7 +13,6 @@ export default function Canvas() {
 
     useEffect(() => {
 
-
         class ThreejsInstance {
             constructor() {
                 this._Init()
@@ -24,18 +22,20 @@ export default function Canvas() {
                 this._scene = new THREE.Scene()
                 this._threejs = new THREE.WebGLRenderer()
                 this._camera = new THREE.PerspectiveCamera(75, canvasParentRef.current.clientWidth / canvasParentRef.current.clientHeight, 0.1, 1000)
-                this._camera.position.set(0, 0, 1.35);
+                this._camera.position.set(0, 0, 2.75);
                 this._threejs.setSize(canvasParentRef.current.clientWidth, canvasParentRef.current.clientHeight)
                 canvasParentRef.current.appendChild( this._threejs.domElement );
-
                 this._orbitControls = new OrbitControls(this._camera, canvasParentRef.current)
                 this._orbitControls.update()
+                this._clock = new THREE.Clock()
 
-                this._AddBoxGeometry()
+                this._LoadModel()
 
                 // CANVAS CHARACTERISTICS
 
                 this._scene.background = new THREE.Color(0x1b264f)
+                const light = new THREE.AmbientLight(0xffffff, 2)
+                this._scene.add(light)
 
                 window.addEventListener('resize', () => {
                     this._OnWindowResize()
@@ -44,12 +44,19 @@ export default function Canvas() {
                 this._RAF()
             }
 
-            _AddBoxGeometry() {
-                const geometry = new THREE.BoxGeometry(1, 1, 1)
-                const material = new THREE.MeshBasicMaterial({ color: 0x8a0c1b })
-                this._box = new THREE.Mesh(geometry, material)
-                this._box.position.setX(-0.5)
-                this._scene.add(this._box)
+            _LoadModel() {
+                const loader = new GLTFLoader()
+                loader.load('assets/yabcube.glb', (gltf) => {
+                    const cube = gltf.scene
+                    cube.position.set(-1, -1, 0.25)
+                    this._mixer = new THREE.AnimationMixer(cube)
+                    this._scene.add(cube)
+                    const clip = gltf.animations.find(clip => clip.name === 'Float')
+                    const action = this._mixer.clipAction(clip)
+                    this._mixer.update(this._clock.getDelta())
+                    action.loop = THREE.LoopRepeat
+                    action.play()
+                })
             }
 
             _OnWindowResize() {
@@ -59,12 +66,12 @@ export default function Canvas() {
                 this._threejs.setSize(canvasParentRef.current.clientWidth, canvasParentRef.current.clientHeight);   
             }
 
-
             _RAF() {
                 requestAnimationFrame(() => {
                     this._threejs.render(this._scene, this._camera)
-                    this._box.rotateX(0.02)
-                    this._box.rotateY(0.01)
+                    if (this._mixer) {
+                        this._mixer.update(this._clock.getDelta())
+                    }
                     this._orbitControls.update()
                     this._RAF()
                 })
