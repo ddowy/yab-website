2c667846159308011fa2b59583a80dc8f4cc4326
ddowy <92402470+ddowy@users.noreply.github.com>
2023-07-30 18:33:19 -0500

    Added in next and prev buttons for cube carousel. Buttons are font awesome icons so all packages needed are now added to package.json.

--- src/components/CubeImageDisplay/CubeImageDisplay.js
+++ src/components/CubeImageDisplay/CubeImageDisplay.js
@@ -1,10 +1,16 @@
 import styles from './cubeImageDisplay.module.css'
 import Canvas from './components/Canvas/Canvas'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleArrowLeft, faCircleArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 export default function CubeImageDisplay() {
     return (
         <div className={styles.container}>
             <Canvas/>
+            <div className={styles.btnContainer}>
+                <FontAwesomeIcon className={styles.prevBtn} icon={faCircleArrowLeft}/>
+                <FontAwesomeIcon className={styles.nextBtn} icon={faCircleArrowRight}/>
+            </div>
         </div>
     )
 }
